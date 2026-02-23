00f7c796a5072f2d1f8fb649920c1e8e01575d64
toddfordham13 <toddfordham13@gmail.com>
2026-02-23 17:54:11 +0000

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,13 @@
 import type { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://graze-lounge.com"; // change later
-
-  return [
-    { url: `${baseUrl}/`, lastModified: new Date() },
-    { url: `${baseUrl}/menu`, lastModified: new Date() },
-    { url: `${baseUrl}/gallery`, lastModified: new Date() },
-  ];
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://graze-lounge.com/sitemap.xml",
+  };
 }
