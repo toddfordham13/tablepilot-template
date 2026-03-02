2079096c12e37c674aa791413222697530c216ca
toddfordham13 <toddfordham13@gmail.com>
2026-03-02 18:29:50 +0000

    Fix sitemap base URL

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://graze-lounge.com";
+  const baseUrl = "https://grazelounge.com";
 
   return [
     {
